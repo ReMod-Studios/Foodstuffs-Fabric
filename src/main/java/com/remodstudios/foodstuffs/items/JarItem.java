39d1905d802453d1549a07f9c93adf88dcfd617a
LePichu <ishat.prsnl65844@gmail.com>
2021-02-14 16:05:14 +0530

    jaritem

---
+++ src/main/java/com/remodstudios/foodstuffs/items/JarItem.java
@@ -0,0 +1,8 @@
+package com.remodstudios.foodstuffs.items;
+import net.minecraft.item.Item;
+
+public class JarItem extends Item {
+    public JarItem(Settings settings) {
+		super(settings);
+    }
+}
